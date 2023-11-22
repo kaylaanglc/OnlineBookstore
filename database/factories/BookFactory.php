c2325eb960f057a038c3cbbddee099879f449968
Khairiya Maisa Putri <caca@Khairiyas-MacBook-Pro.local>
2023-11-22 20:24:01 +0700

    adding more book titles and authors, modifying the book seeder so the book datas are taken randomly

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -18,6 +18,7 @@ class BookFactory extends Factory
      * @return array<string, mixed>
      */
     protected $model = Book::class;
+    private static $counter = 0;
 
     public function definition()
     {
@@ -26,6 +27,26 @@ class BookFactory extends Factory
             'To Kill a Mockingbird',
             '1984',
             'The Great Gatsby',
+            'The Plague Dogs',
+            'Outliers: The Story of Success',
+            'Great River of the Abyss',
+            'No Country for Old Men',
+            'All the Pretty Horses',
+            'Alyzon Whitestarr',
+            'The Ballad of Songbirds and Snakes',
+            'The Hunger Games',
+            'Catching Fire',
+            'Mockingjay',
+            'The Maze Runner',
+            'The Scorch Trials',
+            'The Death Cure',
+            'Divergent',
+            'Allegiant',
+            'Insurgent',
+            'It Ends with Us',
+            'It Starts with Us',
+            'All Your Perfects',
+            'Ugly Love',
         ];
 
         $authorExamples = [
@@ -33,18 +54,39 @@ class BookFactory extends Factory
             'Harper Lee',
             'George Orwell',
             'F. Scott Fitzgerald',
+            'Richard Adams',
+            'Malcolm Gladwell',
+            'Duncan Wilson',
+            'Cormac McCarthy',
+            'Ruth Ozeki',
+            'Isobelle Carmody',
+            'Suzanne Collins',
+            'James Dashner',
+            'Veronica Roth',
+            'Colleen Hoover',
+            'Stephen King',
+            'J. K. Rowling',
+            'Jeff Kinney',
+            'Holly Black',
+            'Kazuo Ishiguro',
+            'Patrick Rothfuss',
+            'Charlie Jane Anders',
+            'Tahereh Mafi',
+            'J. Storer Clouston',
+            'Conor McPherson',
         ];
 
         $price = $this->faker->randomFloat(2, 7, 30); // Random price between 50 and 500.
 
         $isbn = $this->faker->unique()->isbn13;
 
+        self::$counter++;
+
         return [
-            'title' => $this->faker->randomElement($titleExamples),
-            'author' => $this->faker->randomElement($authorExamples),
+            'title' => $titleExamples[self::$counter - 1],
+            'author' => $authorExamples[self::$counter - 1],
             'ISBN' => $isbn,
             'price' => $price,
         ];
     }
 }
-
--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -12,6 +12,6 @@ class BookSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Book::factory(50)->create();
+        \App\Models\Book::factory(24)->create();
     }
 }
