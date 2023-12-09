490022e8716f59eb0325e80fb172d18879f78c38
Khairiya Maisa Putri <caca@Khairiyas-MacBook-Pro.local>
2023-12-09 17:56:30 +0700

    search bar is working but there's still some error huft

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,17 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     public function search(Request $request)
-{
-   $query = $request->get('title');
-
-   $books = Book::where('title', 'like', "%{$query}%")->get();
-
-   return view('books.search', compact('books'));
-}
-
+    {
+        $query = $request->input('title');
+        $books = Book::where('title', 'like', '%' . $query . '%')->get();
+        return view('books.search', compact('books'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,7 +23,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/books/search', 'BookController@search')->name('books.search');
+Route::get('/search', [BookController::class, 'search'])->name('books.search');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
