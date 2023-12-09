a8b4b2f9e5de610e6c897983da1ca22e2097609b
Khairiya Maisa Putri <caca@Khairiyas-MacBook-Pro.local>
2023-12-10 02:59:38 +0700

    search output card and not fixed yet, + footer

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -10,7 +10,13 @@ class BookController extends Controller
     public function search(Request $request)
     {
         $query = $request->input('title');
-        $books = Book::where('title', 'like', '%' . $query . '%')->get();
+        $books = $query = $request->input('title');
+        $books = Book::where('title', 'like', "%$query%")
+                ->orWhere('author', 'like', "%$query%")
+                ->orWhere('isbn', 'like', "%$query%")
+                ->orWhere('price', 'like', "%$query%")
+                ->get();
+
         return view('books.search', compact('books'));
     }
 }
--- resources/views/books/search.blade.php
+++ resources/views/books/search.blade.php
@@ -1,43 +1,44 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        @include('layouts.navigation')
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}                       
-                    </div> 
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
+        <!-- Page Content -->
+        <main class="mt-16 mb-8">
+            <div class="grid grid-cols-2 gap-3">
                 @if(count($books) > 0)
-                @foreach($books as $book)
-                    <p class="{{ config('app.theme') === 'dark' ? 'text-black' : 'text-white' }}">{{ $book->title }}</p>
-                @endforeach
-            @else
-                <p class="{{ config('app.theme') === 'dark' ? 'text-black' : 'text-white' }}">No books found</p>
-            @endif
-            </main>
-        </div>
-    </body>
-</html>
+                    @foreach($books as $book)
+                        <div class="col-span-2 bg-white dark:bg-gray-800 shadow p-4 rounded-md w-full md:w-1/2 lg:w-1/2">
+                            <h2 style="color: {{ config('app.theme') === 'dark' ? 'black' : 'white' }}">{{ $book->title }}</h2>
+                            <p style="color: {{ config('app.theme') === 'dark' ? 'black' : 'white' }}">Author: {{ $book->author }}</p>
+                            <p style="color: {{ config('app.theme') === 'dark' ? 'black' : 'white' }}">ISBN: {{ $book->isbn }}</p>
+                            <p style="color: {{ config('app.theme') === 'dark' ? 'black' : 'white' }}">Price: {{ $book->price }}</p>
+                        </div>
+                    @endforeach
+                @else
+                    <p style="color: {{ config('app.theme') === 'dark' ? 'black' : 'white' }}">No books found</p>
+                @endif
+            </div>
+        </main>
 
+        <!-- Footer Section -->
+        <footer class="bg-gray-800 text-white p-4 text-center">
+            <p>&copy; {{ date('Y') }} CCKK Online Bookstore. All rights reserved.</p>
+        </footer>
+    </div>
+</body>
+</html>
